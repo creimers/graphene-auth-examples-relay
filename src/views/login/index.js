2d2be535b5f1c8e74ec90734ded2ccc0247b5138
Christoph Reimers <christoph@superservice-international.com>
2017-05-30 08:40:04 +0200

    found a way to login a user

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
+import Relay from 'react-relay'
+
+import LoginForm from '../../components/LoginForm'
+import LoginUser from '../../mutations/LoginUser'
 
 class Login extends Component {
+
+  handleLogin(user) {
+    console.log('user: ', user)
+    Relay.Store.commitUpdate(
+      new LoginUser(user),
+      {
+        onSuccess: (resp) => {
+          console.log('login success: ', resp)
+        },
+        onFailure: (transaction) => console.log(transaction)
+      }
+    )
+  }
+
   render() {
     return (
-      <div>So you want to log in?</div>
+      <div>
+        <LoginForm onLogin={this.handleLogin.bind(this)} />
+      </div>
     )
   }
 }
